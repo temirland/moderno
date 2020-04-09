6990035bafaabc01e6dc3029770f06b19c3656ed
temirland <rakanov.t@gmail.com>
2020-04-09 12:49:12 +0600

    added product slider

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,8 @@ gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/rateyo/src/jquery.rateyo.css'
     ])
 
     .pipe(concat('libs.min.css'))   // Файлы выше будут в объеденены в один (libs.min.js)
@@ -37,7 +38,8 @@ gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        'node_modules/mixitup/dist/mixitup.js'
+        'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/rateyo/src/jquery.rateyo.js'
     ])
 
     .pipe(concat('libs.min.js'))   // Файлы выше будут в объеденены в один (libs.min.js)
