e8fc624a5e506c54887f74f9bc445bbb9a063bf9
temirland <rakanov.t@gmail.com>
2020-04-20 19:02:18 +0600

    added login fancybox

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,8 @@ gulp.task('style', function(){
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/magnific-popup/dist/magnific-popup.css',
         'node_modules/rateyo/src/jquery.rateyo.css',
-        'node_modules/ion-rangeslider/css/ion.rangeSlider.css'
+        'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
     ])
 
     .pipe(concat('libs.min.css'))   // Файлы выше будут в объеденены в один (libs.min.js)
@@ -41,7 +42,8 @@ gulp.task('script', function(){
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
-        'node_modules/ion-rangeslider/js/ion.rangeSlider.js'
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     ])
 
     .pipe(concat('libs.min.js'))   // Файлы выше будут в объеденены в один (libs.min.js)
